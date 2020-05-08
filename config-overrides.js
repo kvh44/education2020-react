f9952e677f5fd655a478fd3164ddd062f57e27b0
cheng <bryat.qin@gmail.com>
2020-05-08 12:31:30 +0200

    .gitignore is now working

---
+++ config-overrides.js
@@ -0,0 +1,5 @@
+
+
+const { removeModuleScopePlugin } = require('customize-cra')
+
+module.exports = removeModuleScopePlugin()
---
+++ src/config/APIConfig.js
@@ -0,0 +1,6 @@
+
+export const API_FRANCE_REGION_LIST = 'api/v1/region/list';
+
+export const API_FRANCE_DEPARTMENT_LIST_BY_REGION = 'api/v1/department/list/by-region-code/';
+
+export const API_FRANCE_CITY_LIST_BY_DEPARTMENT = '/api/v1/city/list/by-department-code/';
